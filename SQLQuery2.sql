bc89904ead384fa6738781666e470b525f91b49a
Kacpeee <kacper3490@gmail.com>
2025-10-15 09:53:31 +0200

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,51 +1,50 @@
--- a)
+a
 SELECT id_pracownika, nazwisko FROM ksiegowosc.pracownicy;
 
--- b)
+ b
 SELECT w.id_pracownika
 FROM ksiegowosc.wynagrodzenie w
 JOIN ksiegowosc.pensja p ON w.id_pensji = p.id_pensji
 WHERE p.kwota > 1000;
-
--- c)
+ c
 SELECT w.id_pracownika
 FROM ksiegowosc.wynagrodzenie w
 JOIN ksiegowosc.pensja p ON w.id_pensji = p.id_pensji
 WHERE w.id_premii IS NULL AND p.kwota > 2000;
 
--- d)
+d
 SELECT * FROM ksiegowosc.pracownicy WHERE imie LIKE 'J%';
 
--- e)
+e
 SELECT * FROM ksiegowosc.pracownicy WHERE nazwisko LIKE '%n%' AND imie LIKE '%a';
 
--- f)
+f
 SELECT p.imie, p.nazwisko, (g.liczba_godzin - 160) AS nadgodziny
 FROM ksiegowosc.pracownicy p
 JOIN ksiegowosc.godziny g ON p.id_pracownika = g.id_pracownika;
 
--- g)
+ g
 SELECT p.imie, p.nazwisko
 FROM ksiegowosc.pracownicy p
 JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
 JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 WHERE pe.kwota BETWEEN 1500 AND 3000;
 
--- h)
+ h
 SELECT p.imie, p.nazwisko
 FROM ksiegowosc.pracownicy p
 JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
 JOIN ksiegowosc.godziny g ON w.id_godziny = g.id_godziny
 WHERE g.liczba_godzin > 160 AND w.id_premii IS NULL;
 
--- i)
+i
 SELECT p.imie, p.nazwisko, pe.kwota AS pensja
 FROM ksiegowosc.pracownicy p
 JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
 JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 ORDER BY pe.kwota;
 
--- j)
+j
 SELECT p.imie, p.nazwisko, pe.kwota AS pensja, ISNULL(pr.kwota,0) AS premia
 FROM ksiegowosc.pracownicy p
 JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
@@ -53,40 +52,41 @@ JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 LEFT JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii
 ORDER BY pe.kwota DESC, ISNULL(pr.kwota,0) DESC;
 
--- k)
+k
 SELECT pe.stanowisko, COUNT(*) AS liczba_pracownikow
 FROM ksiegowosc.wynagrodzenie w
 JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 GROUP BY pe.stanowisko;
 
--- l)
+l
 SELECT AVG(pe.kwota) AS srednia, MIN(pe.kwota) AS min, MAX(pe.kwota) AS max
 FROM ksiegowosc.pensja pe
 WHERE pe.stanowisko = 'Kierownik';
 
--- m)
+m
 SELECT SUM(pe.kwota + ISNULL(pr.kwota,0)) AS suma_wynagrodzen
 FROM ksiegowosc.wynagrodzenie w
 JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 LEFT JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii;
 
--- n)
+n
 SELECT pe.stanowisko, SUM(pe.kwota + ISNULL(pr.kwota,0)) AS suma_wynagrodzen
 FROM ksiegowosc.wynagrodzenie w
 JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 LEFT JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii
 GROUP BY pe.stanowisko;
 
--- o)
+ o
 SELECT pe.stanowisko, COUNT(w.id_premii) AS liczba_premii
 FROM ksiegowosc.wynagrodzenie w
 JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 GROUP BY pe.stanowisko;
 
--- p)
+p
 DELETE p
 FROM ksiegowosc.pracownicy p
 JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
 JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
 WHERE pe.kwota < 1200;
+
 GO
